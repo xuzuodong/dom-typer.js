dabcf264ebf308f8ddff34841f51ebe8cda063ce
heoizokdung <xzd333333@163.com>
2020-12-31 22:47:11 +0800

    fix: error when the second peremeter doesn't exist

--- README.md
+++ README.md
@@ -25,33 +25,32 @@ import Typer from "dom-typer";
 ### Basic Use
 
 ```javascript
-let output = "";
 const typer = new Typer("I will be output letter by letter.");
 typer.type((o) => {
-  output = o;
+  console.log(o);
 });
 
 // you can reuse later by doing this
-typer.reset("Other string that you want to output.").type((o) => {
-    console.log(o)
+typer.targetString = "Other string that you want to output.";
+typer.type((o) => {
+  console.log(o);
 });
 
 // alternatively, you can use callback to do something when entire process finished
 typer.type(
-  (currentOutput) => {
-    // will be called every time when one letter finished typing
-  },
+  // first parameter will be called every time when each letter finished typing
+  (currentOutput) => {},
 
+  // second parameter will be called when the whole string finished typing
   (entireOutput) => {
-    // will be called when the whole string finished typing
-    console.log('Typing work finished! ')
+    console.log("Typing work finished! ");
   }
 );
 ```
 
 ### Render DOM
 
-I created this plugin to better show typer style in web-app, so you can also do this to render your styled text. Here is the way to use it in a Vue project:
+I created this plugin to better show typer style in web-app, so you can also do this to render your styled text. Here is an example to use it in a Vue project:
 
 ```html
 <template>
@@ -60,29 +59,31 @@ I created this plugin to better show typer style in web-app, so you can also do 
 ```
 
 ```javascript
+import Typer from "dom-typer";
+
 export default {
   data() {
     return {
-      typer: new Typer("String that you want to render letter by letter."),
       msg: "",
     };
   },
 
   mounted() {
-    this.typer.type((output) => {
-      this.msg = output;
-    });
+    new Typer("String that you want to render letter by letter.").type((o) => {
+      this.msg = o;
+    })
   },
 };
 ```
 
 ### Bold Style
 
-Bold text is also supported. What you need is to wrap the specific text part with `**`, just like Markdown syntax, and then the text rendered in your web-app will become bold style.
+Bold text is also supported. What you need is to wrap the specific text part with `**`, just like MarkDown syntax, and then the text rendered in your web-app will become bold style.
 
 ```javascript
-const typer = new Typer("String that you want to **accent**. ");
+new Typer("String that you want to partly **accent**. ").type((o) => {
+  // receive the output string with bold part wrapped with <strong></strong> markup tag 
+});
 ```
 
 ![](./static/accent.gif)
-
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-import marked from 'marked'
+import marked from "marked";
 
 class Typer {
   constructor(str) {
-    this.targetString = str
-    this.outputString = ''
-    this.speed = 50
-    this.typing = false
-    this.lexicalStructure = {}
-    this.pointer = [0, 0] // 正在打印的位置，位于第几个节点的第几个字
+    this.targetString = str ? str : "";
+    this.outputString = "";
+    this.speed = 50;
+    this.typing = false;
+    this.lexicalStructure = str ? marked.lexer(str) : {};
+    this.pointer = [0, 0];
+  }
+
+  set targetString(str) {
+    this.outputString = "";
+    this.typing = false;
+    this.lexicalStructure = marked.lexer(str);
+    this.pointer = [0, 0];
   }
 
   /**
@@ -16,17 +23,17 @@ class Typer {
    * @param done 完成整个字符串的回调函数，可拿到最终字符串
    */
   type(tick, done) {
-    const nodesLength = this.lexicalStructure[0].tokens.length // 节点数量
-    const currentNode = this.lexicalStructure[0].tokens[this.pointer[0]] // 当前节点
-    const currentNodeIndex = this.pointer[0] // 当前是第几个节点
-    const currentTextLength = currentNode.text.length // 当前节点文本长度
-    const currentTextIndex = this.pointer[1] // 当前是文本的第几个字符
-    this.typing = true
+    const nodesLength = this.lexicalStructure[0].tokens.length; // 节点数量
+    const currentNode = this.lexicalStructure[0].tokens[this.pointer[0]]; // 当前节点
+    const currentNodeIndex = this.pointer[0]; // 当前是第几个节点
+    const currentTextLength = currentNode.text.length; // 当前节点文本长度
+    const currentTextIndex = this.pointer[1]; // 当前是文本的第几个字符
+    this.typing = true;
 
-    if (currentNode.type == 'text') {
-      this.outputString += currentNode.text[currentTextIndex]
-    } else if (currentNode.type == 'strong') {
-      this.outputString += `<strong>${currentNode.text[currentTextIndex]}</strong>`
+    if (currentNode.type == "text") {
+      this.outputString += currentNode.text[currentTextIndex];
+    } else if (currentNode.type == "strong") {
+      this.outputString += `<strong>${currentNode.text[currentTextIndex]}</strong>`;
     }
 
     // 如果不在最后一个节点，也不是最后一个字符，则 pointer[1]++
@@ -37,42 +44,41 @@ class Typer {
     // 只要不是最后一个字符，那就只需要执行 pointer[1]++；
     // 如果是最后一个字符，则判断是不是最后一个节点里的最后一个字符。
     if (currentTextIndex + 1 != currentTextLength) {
-      this.pointer[1]++
+      this.pointer[1]++;
     }
 
     // 不是最后一个节点中的最后一个字符，则将 pointer 移动到下一个节点的 text 头部
-    else if (currentTextIndex + 1 == currentTextLength && currentNodeIndex + 1 != nodesLength) {
-      this.pointer[0]++
-      this.pointer[1] = 0
+    else if (
+      currentTextIndex + 1 == currentTextLength &&
+      currentNodeIndex + 1 != nodesLength
+    ) {
+      this.pointer[0]++;
+      this.pointer[1] = 0;
     }
 
     // 剩下的情况就是最后一个节点的最后一个字符
     else {
-      this.pointer[0]++
-      this.pointer[1]++
+      this.pointer[0]++;
+      this.pointer[1]++;
     }
 
-    tick(this.outputString)
+    if (tick) tick(this.outputString);
 
     setTimeout(() => {
-      // 判断本次打印是否完成了全部任务
-      if (this.pointer[0] != nodesLength && this.pointer[1] != currentTextLength) {
-        this.type(tick, done)
-      } else {
-        this.typing = false
-        done(this.outputString)
-      }
-    }, this.speed)
-  }
+      if (!this.typing) return;
 
-  reset(str) {
-    this.targetString = str
-    this.lexicalStructure = marked.lexer(str)
-    this.pointer = [0, 0]
-    this.outputString = ''
-    this.typing = false
-    return this
+      // 判断本次打印是否完成了全部任务
+      if (
+        this.pointer[0] != nodesLength &&
+        this.pointer[1] != currentTextLength
+      ) {
+        this.type(tick, done);
+      } else {
+        this.typing = false;
+        if (done) done(this.outputString);
+      }
+    }, this.speed);
   }
 }
 
-export default Typer
+export default Typer;
